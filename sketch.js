7a761547ffc622cbe3c8a7fcceb2d6e3d1b6f584
Oded Welgreen <oded.welgreen@gmail.com>
2023-12-10 11:34:04 -0600

    another color sketch iteration

--- sketch.js
+++ sketch.js
@@ -2,18 +2,34 @@
 /// <reference path="./src/index.ts" />
 
 // constants
-// const RADIUS = 100;
+const MANUAL_FREQUENCY = 3 * 60;
+const NUM_LFOS = 100;
+const FREQUENCY = 60;
 
 // locals
 const points = [];
 let lfo1, lfo2, lfo3;
+let circleLfo;
+let radius = 0;
+let size = 1;
+const lfos = [];
 
 function setup() {
   createCanvas(600, 600);
-  stroke(255);
+  // stroke(255);
+  noStroke();
+  fill(255);
   rectMode(CENTER);
   textAlign(CENTER, CENTER);
-  //   background(0);
+  for (let i = 0; i < NUM_LFOS; i++) {
+    const phase = (i * FREQUENCY) / NUM_LFOS;
+    lfos.push(createLfo(LfoWaveform.Sawtooth, LfoTiming.Frames(60), 0, TWO_PI, phase));
+  }
+  // circleLfo = createLfo(LfoWaveform.Sawtooth, LfoTiming.Frames(60), 0, TWO_PI);
+  background(0);
+  // lfo1 = createLfo(LfoWaveform.Sine, LfoTiming.Manual(MANUAL_FREQUENCY));
+  // lfo2 = createLfo(LfoWaveform.Sine, LfoTiming.Frames(MANUAL_FREQUENCY / 2));
+
   lfo1 = createLfo(LfoWaveform.Sine, LfoTiming.Frames(12 * 60));
   lfo2 = createLfo(LfoWaveform.Sine, LfoTiming.Frames(6 * 60));
   lfo3 = createLfo(LfoWaveform.Sine, LfoTiming.Frames(4 * 60));
@@ -22,42 +38,28 @@ function setup() {
 
 function draw() {
   // background(0);
-  noStroke();
+
   const l = map(lfo1.value, -1, 1, 0, 1);
   const a = map(lfo2.value, -1, 1, -0.4, 0.4);
   const b = map(lfo3.value, -1, 1, -0.4, 0.4);
+
   let color = new Color('oklab', [l, a, b]);
+  fill(color.srgb.r * 255, color.srgb.g * 255, color.srgb.b * 255);
 
-  background(color.srgb.r * 255, color.srgb.g * 255, color.srgb.b * 255);
-  const x1 = 0.3 * width;
-  const y1 = map(lfo1.value, -1, 1, 0.7 * height, 0.3 * height);
-  const x2 = 0.5 * width;
-  const y2 = map(lfo2.value, -1, 1, 0.7 * height, 0.3 * height);
-  const x3 = 0.7 * width;
-  const y3 = map(lfo3.value, -1, 1, 0.7 * height, 0.3 * height);
+  // const r = map(lfo1.value, -1, 1, 0, 255);
+  // const g = map(lfo2.value, -1, 1, 0, 255);
+  // const b = map(lfo3.value, -1, 1, 0, 255);
+  // fill(r, g, b);
+  translate(width / 2, height / 2);
 
-  fill(0);
-  rect(x1, width / 2, 20, 0.4 * height);
-  rect(x2, width / 2, 20, 0.4 * height);
-  rect(x3, width / 2, 20, 0.4 * height);
-  fill(100);
-  stroke(0);
-  strokeWeight(1);
-  rect(x1, y1, 50, 20);
-  rect(x2, y2, 50, 20);
-  rect(x3, y3, 50, 20);
+  for (const lfo of lfos) {
+    const { x, y } = polarToCartesian(radius, lfo.value);
+    ellipse(x, y, size, size);
+    lfo.frequency.value += 0.01;
+  }
 
-  fill(255);
-  stroke(0);
-  strokeWeight(2);
-  textSize(13);
-  // text(int(r.toString()), x1, y1);
-  // text(int(g.toString()), x2, y2);
-  // text(int(b.toString()), x3, y3);
-  textSize(30);
-  text('Red', x1, 0.75 * height);
-  text('Green', x2, 0.75 * height);
-  text('Blue', x3, 0.75 * height);
+  radius += 1;
+  size += 0.1;
 }
 
 let isLooping = true;
