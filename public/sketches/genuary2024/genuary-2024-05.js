a0c0b9ba7b3e28ed203d3f77e7fa680f8e0f761a
Oded Welgreen <oded.welgreen@gmail.com>
2024-01-02 22:28:20 -0600

    finish genuary 5

--- public/sketches/genuary2024/genuary-2024-05.js
+++ public/sketches/genuary2024/genuary-2024-05.js
@@ -1,22 +1,66 @@
 /// <reference types="p5/global" />
 
 // constants
-// const RADIUS = 100;
-const SIZE = 100;
+const SIZE = (75 * 7) / 6;
+const NUM_SHAPES_PER_ROW = 5;
+const PADDING = (70 * 7) / 6;
 const SNAPSHOT_INTERVAL = 10;
-const FREQUENCY = 120;
+const FREQUENCY = 240;
 const MAX_SNAPSHOTS = 10;
+const PHASE_RATIO = 2;
 
 // locals
 let brLfo, xLfo, yLfo, bezierLfo, colorLfo, diamondLfo;
 let stage = 'square';
 let stageFuncs = {};
 const snapshots = [];
+const shapes = [];
+const palette = ['#c06046', '#8c2336', '#a9413e'];
+class Shape {
+  constructor(x, y, phase, size, color) {
+    this.x = x;
+    this.y = y;
+    this.stage = 'square';
+    this.size = size || SIZE;
+    this.phase = phase;
+    this.createShapeLfos(phase);
+    this.color = color;
+  }
 
-function createShapeLfos() {
-  brLfo = createLfo(LfoWaveform.Triangle, Timing.frames(FREQUENCY, FREQUENCY * 0.75), 0, SIZE / 2);
-  bezierLfo = createLfo(LfoWaveform.Triangle, Timing.frames(FREQUENCY, FREQUENCY * 0.75), 0, 1);
-  diamondLfo = createLfo(LfoWaveform.Triangle, Timing.frames(FREQUENCY, FREQUENCY * 0.75), 0, 1);
+  createShapeLfos(phase = 0) {
+    this.brLfo = createLfo(LfoWaveform.Triangle, Timing.frames(FREQUENCY, FREQUENCY * 0.75 + phase), 0, this.size / 2);
+    this.bezierLfo = createLfo(LfoWaveform.Triangle, Timing.frames(FREQUENCY, FREQUENCY * 0.75), 0, 1);
+    this.diamondLfo = createLfo(LfoWaveform.Triangle, Timing.frames(FREQUENCY, FREQUENCY * 0.75), 0, 1);
+  }
+
+  draw() {
+    fill(this.color);
+    push();
+    translate(this.x, this.y);
+    if (this.stage === 'square') {
+      drawSquare(this.brLfo.value, this.size);
+
+      if (this.brLfo.value >= this.size / 2) {
+        this.stage = 'circle';
+        this.createShapeLfos();
+      }
+    } else if (this.stage === 'circle') {
+      drawCircle(this.bezierLfo.value, this.size);
+
+      if (this.bezierLfo.value >= 1) {
+        this.stage = 'diamond';
+        this.createShapeLfos();
+      }
+    } else if (this.stage === 'diamond') {
+      drawDiamond(this.diamondLfo.value, this.size);
+
+      if (this.diamondLfo.value >= 1) {
+        this.stage = 'square';
+        this.createShapeLfos();
+      }
+    }
+    pop();
+  }
 }
 
 function drawBezierOvalQuarter(sizeX, sizeY, value) {
@@ -26,8 +70,6 @@ function drawBezierOvalQuarter(sizeX, sizeY, value) {
   const cp2x = map(value, 0, 1, -0.552 * sizeX, 0);
   bezierVertex(-sizeX, cp1y, cp2x, -sizeY, 0, -sizeY);
   endShape();
-  // ellipse(-sizeX, cp1y, 10);
-  // ellipse(cp2x, -sizeY, 10);
 }
 
 function drawBezierOval(sizeX, sizeY, value) {
@@ -37,121 +79,68 @@ function drawBezierOval(sizeX, sizeY, value) {
   drawBezierOvalQuarter(-sizeX, -sizeY, value);
 }
 
-function drawCircle(value) {
-  drawBezierOval(SIZE / 2, SIZE / 2, value);
+function drawCircle(value, size) {
+  drawBezierOval(size / 2, size / 2, value);
+  rotate(PI);
+  drawDiamond(0, size + 1);
 }
 
-function stepCircle() {
-  drawCircle(bezierLfo.value);
-
-  if (bezierLfo.value >= 1) {
-    stage = 'diamond';
-    createShapeLfos();
-  }
-
-  return bezierLfo.value;
+function drawSquare(value, size) {
+  square(0, 0, size, value);
 }
 
-function drawSquare(value) {
-  square(0, 0, SIZE, value);
-}
-function stepSquare() {
-  drawSquare(brLfo.value);
-
-  if (brLfo.value >= SIZE / 2) {
-    stage = 'circle';
-    createShapeLfos();
-  }
-
-  return brLfo.value;
-}
-
-function drawDiamond(value) {
+function drawDiamond(value, size) {
   rotate(map(value, 0, 1, PI / 4, PI));
-  square(0, 0, map(value, 0, 1, sqrt((SIZE * SIZE) / 2), SIZE));
+  square(0, 0, map(value, 0, 1, sqrt((size * size) / 2), size));
 }
-
-function stepDiamond() {
-  drawDiamond(diamondLfo.value);
-
-  if (diamondLfo.value >= 1) {
-    stage = 'square';
-    createShapeLfos();
-  }
-
-  return diamondLfo.value;
-}
-
-// 2*x*x = SIZE*SIZE
-// x = sqrt(SIZE*SIZE/2)
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(700, 700);
   stroke(255);
-  strokeWeight(2);
   rectMode(CENTER);
-  noFill();
+  noStroke();
 
-  createShapeLfos();
-  colorLfo = createColorLfo();
-  xLfo = createLfo(LfoWaveform.Sine, Timing.frames(FREQUENCY * 2, FREQUENCY * 0.75), width * 0.25, width * 0.75);
-  yLfo = createLfo(LfoWaveform.Sine, Timing.frames(FREQUENCY * 4, FREQUENCY * 0.75), width * 0.25, width * 0.75);
+  const points = generateGrid(NUM_SHAPES_PER_ROW, NUM_SHAPES_PER_ROW, width - PADDING * 2, height - PADDING * 2);
 
-  stageFuncs = {
-    square: stepSquare,
-    circle: stepCircle,
-    diamond: stepDiamond,
-  };
-  background(0);
+  let index = 0;
+
+  for (let i = points.length - 1; i >= 0; i--) {
+    const point = points[i];
+    shapes.push(new Shape(point.x + PADDING - SIZE / 6, point.y + PADDING, index * PHASE_RATIO, SIZE, palette[0]));
+    shapes.push(new Shape(point.x + PADDING, point.y + PADDING, index * PHASE_RATIO, SIZE, palette[1]));
+    shapes.push(new Shape(point.x + PADDING + SIZE / 6, point.y + PADDING, index * PHASE_RATIO, SIZE, palette[2]));
+    index++;
+  }
+  // noLoop();
 }
 
 function draw() {
-  background(0);
-  // const c = colorLfo.color();
-  // stroke(c.r, c.g, c.b);
-  push();
-  translate(xLfo.value, yLfo.value);
-  // translate(width / 2, height / 2);
-
-  const value = stageFuncs[stage]();
-  pop();
-
-  for (const s of snapshots) {
-    push();
-    translate(s.x, s.y);
-    if (s.stage === 'square') {
-      drawSquare(s.value);
-    } else if (s.stage === 'circle') {
-      drawCircle(s.value);
-    } else if (s.stage === 'diamond') {
-      drawDiamond(s.value);
+  if (frameCount === 1) {
+    if (frameCount === 1) {
+      const capture = P5Capture.getInstance();
+      capture.start({
+        format: 'png',
+        framerate: 30,
+        quality: 1,
+      });
     }
-    pop();
+  }
+  background(238, 243, 248);
+
+  for (const shape of shapes) {
+    shape.draw();
   }
 
-  if (frameCount % SNAPSHOT_INTERVAL === 0) {
-    snapshots.push({
-      x: xLfo.value,
-      y: yLfo.value,
-      size: SIZE,
-      stage,
-      value,
-    });
-    if (snapshots.length > MAX_SNAPSHOTS) {
-      // snapshots.shift();
-    }
+  if (frameCount === FREQUENCY * 1.5) {
+    const capture = P5Capture.getInstance();
+    capture.stop();
+    console.log('done');
+    // capture.download();
   }
 }
 
-let isLooping = true;
 function mouseClicked() {
-  if (isLooping) {
-    noLoop();
-  } else {
-    loop();
-  }
-
-  isLooping = !isLooping;
+  loop();
 }
 
 P5Capture.setDefaultOptions({
--- sketch.js
+++ sketch.js
@@ -1,122 +1,22 @@
 /// <reference types="p5/global" />
 
 // constants
-const SIZE = 75;
-const NUM_SHAPES_PER_ROW = 5;
-const PADDING = 70;
-const SNAPSHOT_INTERVAL = 10;
-const FREQUENCY = 240;
-const MAX_SNAPSHOTS = 10;
-const PHASE_RATIO = 2;
+// const RADIUS = 100;
 
 // locals
-let brLfo, xLfo, yLfo, bezierLfo, colorLfo, diamondLfo;
-let stage = 'square';
-let stageFuncs = {};
-const snapshots = [];
-const shapes = [];
-
-class Shape {
-  constructor(x, y, phase, size) {
-    this.x = x;
-    this.y = y;
-    this.stage = 'square';
-    this.size = size || SIZE;
-    this.phase = phase;
-    this.createShapeLfos(phase);
-  }
-
-  createShapeLfos(phase = 0) {
-    this.brLfo = createLfo(LfoWaveform.Triangle, Timing.frames(FREQUENCY, FREQUENCY * 0.75 + phase), 0, this.size / 2);
-    this.bezierLfo = createLfo(LfoWaveform.Triangle, Timing.frames(FREQUENCY, FREQUENCY * 0.75), 0, 1);
-    this.diamondLfo = createLfo(LfoWaveform.Triangle, Timing.frames(FREQUENCY, FREQUENCY * 0.75), 0, 1);
-  }
-
-  draw() {
-    push();
-    translate(this.x, this.y);
-    if (this.stage === 'square') {
-      drawSquare(this.brLfo.value, this.size);
-
-      if (this.brLfo.value >= this.size / 2) {
-        this.stage = 'circle';
-        this.createShapeLfos();
-      }
-    } else if (this.stage === 'circle') {
-      drawCircle(this.bezierLfo.value, this.size);
-
-      if (this.bezierLfo.value >= 1) {
-        this.stage = 'diamond';
-        this.createShapeLfos();
-      }
-    } else if (this.stage === 'diamond') {
-      drawDiamond(this.diamondLfo.value, this.size);
-
-      if (this.diamondLfo.value >= 1) {
-        this.stage = 'square';
-        this.createShapeLfos();
-      }
-    }
-    pop();
-  }
-}
-
-function drawBezierOvalQuarter(sizeX, sizeY, value) {
-  beginShape();
-  vertex(-sizeX, 0);
-  const cp1y = map(value, 0, 1, -0.552 * sizeY, 0);
-  const cp2x = map(value, 0, 1, -0.552 * sizeX, 0);
-  bezierVertex(-sizeX, cp1y, cp2x, -sizeY, 0, -sizeY);
-  endShape();
-}
-
-function drawBezierOval(sizeX, sizeY, value) {
-  drawBezierOvalQuarter(-sizeX, sizeY, value);
-  drawBezierOvalQuarter(sizeX, sizeY, value);
-  drawBezierOvalQuarter(sizeX, -sizeY, value);
-  drawBezierOvalQuarter(-sizeX, -sizeY, value);
-}
-
-function drawCircle(value, size) {
-  drawBezierOval(size / 2, size / 2, value);
-}
-
-function drawSquare(value, size) {
-  square(0, 0, size, value);
-}
-
-function drawDiamond(value, size) {
-  rotate(map(value, 0, 1, PI / 4, PI));
-  square(0, 0, map(value, 0, 1, sqrt((size * size) / 2), size));
-}
+const points = [];
+let img;
+let lfo1, lfo2, lfo3;
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(700, 700);
   stroke(255);
-  strokeWeight(2);
   rectMode(CENTER);
-  noFill();
-
-  const points = generateGrid(NUM_SHAPES_PER_ROW, NUM_SHAPES_PER_ROW, width - PADDING * 2, height - PADDING * 2);
-
-  let index = 0;
-
-  for (let i = points.length - 1; i >= 0; i--) {
-    const point = points[i];
-    shapes.push(new Shape(point.x + PADDING - SIZE / 8, point.y + PADDING, index * PHASE_RATIO));
-    shapes.push(new Shape(point.x + PADDING, point.y + PADDING, index * PHASE_RATIO));
-    shapes.push(new Shape(point.x + PADDING + SIZE / 8, point.y + PADDING, index * PHASE_RATIO));
-    index++;
-  }
-  background(0);
+  //   background(0);
 }
 
 function draw() {
-  background(0, 0, 0);
-
-  for (const shape of shapes) {
-    shape.draw();
-  }
+  background(0);
 }
 
 let isLooping = true;
