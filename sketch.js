cdc96b135b370f832098243b1088895080265ce2
Oded Welgreen <oded.welgreen@gmail.com>
2024-01-08 20:51:27 -0600

    add rulesets

--- sketch.js
+++ sketch.js
@@ -1,235 +1,217 @@
 /// <reference types="p5/global" />
 
 // constants
-const NUM_COLS = 23;
-const NUM_ROWS = 23;
-const NUM_MOVES = 23;
-const SIZE = 30;
+const SIZE = 3;
+const WIDTH = Math.sqrt(3) * SIZE;
 const HEIGHT = SIZE * 2;
-const WIDTH = Math.round(Math.sqrt(3) * SIZE);
-console.log('===========================');
-console.log(WIDTH, HEIGHT);
-console.log('===========================');
+const FRAME_RATE = 1;
+const DEBUG = false;
 
 // locals
-const particles = [];
-let pg, img;
-let timer;
-let horizontal = true;
-let chosen = [];
-let rowDir;
+const cells = [];
+let RULES = [];
+let palette = [];
 let rows, cols;
-let TILE_SIZE;
-let moves = [];
-let mask;
-
-class Particle {
-  constructor(x, y, s, pg, row, col) {
-    this.x = x;
-    this.y = y;
-    this.w = WIDTH;
-    this.h = HEIGHT;
-    this.s = s;
+let ruleIndex = 0;
+class Cell {
+  constructor(row, col) {
     this.row = row;
     this.col = col;
-    this.dx = 0;
-    this.frame = createGraphics(this.w, this.h);
-    this.frame.image(pg.get(this.x, this.y, this.w, this.h), 0, 0, this.w, this.h);
-
-    this.frame.fill(0);
-    [
-      [
-        [0, 0],
-        [this.w / 2, 0],
-        [0, this.s / 2],
-      ],
-      [
-        [this.w / 2, 0],
-        [this.w, 0],
-        [this.w, this.s / 2],
-      ],
-      [
-        [this.w / 2, this.h],
-        [this.w, this.h],
-        [this.w, this.h - this.s / 2],
-      ],
-      [
-        [0, this.h],
-        [this.w / 2, this.h],
-        [0, this.h - this.s / 2],
-      ],
-    ].forEach((t) => {
-      this.frame.beginShape();
-      t.forEach((v) => this.frame.vertex(v[0], v[1]));
-      this.frame.endShape(CLOSE);
-    });
+    this.state = false;
+    this.nextState = null;
+    this.neighbors = [];
+    const offset = this.row % 2 ? WIDTH / 2 : 0;
+    this.x = this.col * WIDTH + offset;
+    this.y = (this.row * HEIGHT * 3) / 4;
   }
-}
-function preload() {
-  img = loadImage('public/assets/ThePinkCloud_HenriEdmondCross.jpg');
-}
 
-function randomizeStart() {
-  for (let i = 0; i < NUM_MOVES; i++) {
-    randomizeNextMove();
-    moves.push({
-      rowDir: rowDir.map((r) => (r *= -1)),
-      // colDir: colDir.map((c) => (c *= -1)),
-    });
-    for (const p of particles) {
-      p.dx = rowDir[p.row] * TILE_SIZE;
-      // p.dy = colDir[p.col] * TILE_SIZE;
-    }
-    commitMove();
+  update() {
+    this.highlighted = false;
+    this.numLivingNeighbors = this.neighbors.filter((c) => c.state).length;
+    const rules = this.state ? RULES[0] : RULES[1];
+    this.nextState = rules.includes(this.numLivingNeighbors);
   }
-  moves.reverse();
-  // colDir = moves[currentMove].colDir;
-  rowDir = moves[currentMove].rowDir;
-}
 
-function randomizeNextMove() {
-  horizontal = !horizontal;
-  rowDir = Array(rows).fill(0);
-  // colDir = Array(cols).fill(0);
+  commit() {
+    this.state = this.nextState;
+  }
 
-  const numChosen = int(random(1, cols));
+  draw() {
+    if (DEBUG) stroke(255);
+    fill(this.state ? palette[this.numLivingNeighbors - 1] : 0);
+    push();
+    translate(this.x, this.y);
 
-  chosen = [...Array(int(width / TILE_SIZE)).keys()].sort(() => 0.5 - Math.random()).slice(0, numChosen);
-
-  chosen.forEach((c) => {
-    if (horizontal) {
-      // colDir[c] = Math.random() > 0.5 ? -1 : 1;
-    } else {
-      rowDir[c] = Math.random() > 0.5 ? -1 : 1;
+    beginShape();
+    for (let i = 0; i < 6; i++) {
+      const angle = (Math.PI / 3) * i + Math.PI / 6;
+      const x2 = SIZE * Math.cos(angle);
+      const y2 = SIZE * Math.sin(angle);
+      vertex(x2, y2);
     }
-  });
-}
-
-function commitMove() {
-  for (const p of particles) {
-    if (p.dx !== 0) {
-      p.x += WIDTH * rowDir[p.row];
-      p.dx = 0;
-      p.col += rowDir[p.row];
+    endShape(CLOSE);
+    if (DEBUG) {
+      textAlign(CENTER, CENTER);
+      fill(this.highlighted || this.state ? 0 : 255);
+      stroke(this.highlighted || this.state ? 0 : 255);
+      text(`${this.col},${this.row}`, 0, 0);
     }
-    // if (p.dy !== 0) {
-    //   p.y += TILE_SIZE * colDir[p.col];
-    //   p.dy = 0;
-    //   p.row += colDir[p.col];
-    // }
-
-    // wrap around
-    if (p.col === cols) {
-      p.col = 0;
-      p.x = 0;
-    } else if (p.col === -1) {
-      p.col = cols - 1;
-      p.x = width - TILE_SIZE;
-    }
-
-    // if (p.row === rows) {
-    //   p.row = 0;
-    //   p.y = 0;
-    // } else if (p.row === -1) {
-    //   p.row = rows - 1;
-    //   p.y = height - TILE_SIZE;
-    // }
+    pop();
   }
 }
 
-function updateDeltas() {
-  for (const p of particles) {
-    if (rowDir[p.row] !== 0) {
-      p.dx = map(timer.elapsed, 0, 1, 0, WIDTH * rowDir[p.row]);
-      console.log(p.dx);
+function reset() {
+  let row = 0,
+    col = 0;
+
+  if (!cells.length) {
+    // create cells
+    for (let y = 0; y < height + HEIGHT; y += 1.5 * SIZE) {
+      col = 0;
+      for (let x = 0; x < width + WIDTH; x += WIDTH) {
+        cells.push(new Cell(row, col));
+        col++;
+      }
+      row++;
     }
-    // if (colDir[p.col] !== 0) {
-    //   p.dy = map(timer.elapsed, 0, 1, 0, TILE_SIZE * colDir[p.col]);
-    // }
+    rows = row;
+    cols = col;
+
+    // set neighbors
+    for (const c of cells) {
+      const { row, col } = c;
+      const offset = row % 2;
+      const neighbors = [
+        [col - 1, row],
+        [col + 1, row],
+        [col - 1 + offset, row + 1],
+        [col + offset, row + 1],
+        [col - 1 + offset, row - 1],
+        [col + offset, row - 1],
+      ];
+      for (const [col, row] of neighbors) {
+        const neighbor = cells.find((c) => c.row === row && c.col === col);
+        if (neighbor) {
+          c.neighbors.push(neighbor);
+        }
+      }
+    }
+  } else {
+    // reset cells
+    for (const c of cells) {
+      c.state = false;
+      c.nextState = null;
+    }
   }
+  // set ruleset and colors
+  RULES = [RULESETS[ruleIndex][0], RULESETS[ruleIndex][1]];
+  console.log('rule index', ruleIndex);
+  palette = [1, 2, 3, 4, 5, 6].map(() => color(random(255), random(255), random(255)));
+
+  // initialize center
+  const centerCell = cells.find((c) => c.row === int(rows / 2) && c.col === int(cols / 2));
+  centerCell.state = true;
+  centerCell.neighbors.forEach((c) => (c.state = true));
+  centerCell.neighbors.flatMap((c) => c.neighbors).forEach((c) => (c.state = true));
 }
 
 function setup() {
-  createCanvas(600, 600);
-  TILE_SIZE = width / NUM_COLS;
-  stroke(255);
+  createCanvas(800, 800);
   rectMode(CENTER);
   noStroke();
-  pg = createGraphics(600, 600);
-  pg.image(img, 0, 0, width, height);
-  blendMode(LIGHTEST);
-
-  cols = 0;
-  for (let y = 0; y < height; y += 1.5 * SIZE - 1) {
-    rows = 0;
-    for (let x = 0; x < width; x += WIDTH) {
-      const offset = cols % 2 === 0 ? 0 : WIDTH / 2;
-      particles.push(new Particle(x - offset, y, SIZE, pg, cols, rows));
-      rows++;
-    }
-    cols++;
-  }
-
-  // particles.push(new Particle(0, 0, SIZE, pg, 0, 0));
-  // randomizeStart();
-
-  // fill directions with zeros
-
-  // moves.push({
-  //   rowDir: Array(cols).fill(1),
-  // });
-  rowDir = Array(rows).fill(0);
-  rowDir[2] = 1;
-
-  timer = Timing.frames(30, 0, false);
-  //   background(0);
+  reset();
+  background(0);
 }
 
-let currentMove = 0;
 function draw() {
-  blendMode(NORMAL);
-
-  background(0);
-  blendMode(LIGHTEST);
-
-  for (const p of particles) {
-    image(p.frame, p.x + p.dx, p.y, p.frame.width, p.frame.height);
-
-    // render wrap around
-    if (p.x + p.dx + WIDTH > width) {
-      // image(p.frame, p.x + p.dx - width + WIDTH / 2, p.y, WIDTH, HEIGHT);
+  if (frameCount % FRAME_RATE === 0) {
+    background(0);
+    for (const c of cells) {
+      c.update();
     }
-    // if (p.row === rows - 1) {
-    //   image(p.img, p.x + p.dx, p.y + p.dy - height, WIDTH, HEIGHT);
-    // } else if (p.row === 0) {
-    //   image(p.img, p.x + p.dx, p.y + p.dy + height, WIDTH, HEIGHT);
-    // }
-  }
-
-  if (timer.finished) {
-    commitMove();
-    // currentMove++;
-    // if (currentMove < moves.length) {
-    //   colDir = moves[currentMove].colDir;
-    //   rowDir = moves[currentMove].rowDir;
-    //   timer.reset();
-    // }
-  } else {
-    updateDeltas();
+    for (const c of cells) {
+      c.commit();
+      c.draw();
+    }
   }
 }
 
 let isLooping = true;
 function mouseClicked() {
-  if (isLooping) {
-    noLoop();
-  } else {
-    loop();
+  // find the cell that was clicked
+  // if (isLooping) {
+  //   noLoop();
+  // } else {
+  //   loop();
+  // }
+  // isLooping = !isLooping;
+  const cell = cells.find((c) => {
+    const d = dist(mouseX, mouseY, c.x, c.y);
+    return d < SIZE;
+  });
+  if (cell) {
+    // cell.highlighted = true;
+    // cell.neighbors.forEach((c) => (c.highlighted = true));
+    cell.state = !cell.state;
   }
+}
 
-  isLooping = !isLooping;
+function keyPressed() {
+  if (key === ' ') {
+    ruleIndex = (ruleIndex + 1) % RULESETS.length;
+    reset();
+  } else if (key === '1') {
+    const centerCell = cells.find((c) => c.row === int(rows / 2) && c.col === int(cols / 2));
+    centerCell.state = true;
+
+    centerCell.neighbors.forEach((c) => {
+      c.update();
+    });
+    centerCell.neighbors.forEach((c) => {
+      c.commit();
+    });
+  } else if (key === '2') {
+    const centerCell = cells.find((c) => c.row === int(rows / 2) && c.col === int(cols / 2));
+    centerCell.state = true;
+
+    centerCell.neighbors.forEach((c) => (c.state = true));
+
+    new Set([centerCell, ...centerCell.neighbors, ...centerCell.neighbors.flatMap((c) => c.neighbors)]).forEach((c) => {
+      c.update();
+    });
+
+    new Set([centerCell, ...centerCell.neighbors, ...centerCell.neighbors.flatMap((c) => c.neighbors)]).forEach((c) => {
+      c.commit();
+    });
+  }
 }
 
 P5Capture.setDefaultOptions({
   disableUi: true,
 });
+
+const RULESETS = [
+  [
+    [4, 3, 2, 6, 5],
+    [4, 2],
+  ],
+  [
+    [3, 2, 1],
+    [4, 3, 2],
+  ],
+  [[4, 5, 1], [2]],
+  [
+    [6, 5, 4, 3, 2],
+    [1, 5, 6],
+  ],
+  [[4, 3, 2], [1]],
+  [
+    [5, 4, 3],
+    [1, 2, 3],
+  ],
+  [[1, 5, 4], [1]],
+  [
+    [1, 5, 2, 4, 3],
+    [1, 2, 3, 6],
+  ],
+];
