2805462e74736d42be48657399f96e1377c556fa
Oded Welgreen <oded.welgreen@gmail.com>
2024-10-20 18:29:33 -0400

    test sketch

--- sketch.js
+++ sketch.js
@@ -3,17 +3,21 @@
 // constants
 
 // locals
-
+let lfo1;
 function setup() {
   createCanvas(700, 700);
   stroke(255);
   rectMode(CENTER);
+  lfo1 = createLfo(LfoWaveform.Sine, Timing.frames(120), -PI, PI);
   // pixelDensity(1);
   //   background(0);
 }
 
 function draw() {
   background(0);
+  translate(width / 2, height / 2);
+  rotate(lfo1.value);
+  rect(0, 0, 100, 100);
 }
 
 let isLooping = true;
