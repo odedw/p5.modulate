45e96ad440395e9fc08b4f3146bab3636783e3b7
Oded Welgreen <oded.welgreen@gmail.com>
2024-10-20 18:52:59 -0400

    tweak build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,19 +6,14 @@ export default defineConfig({
   publicDir: false,
   build: {
     lib: {
-      // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'main.ts'),
-      name: 'p5.modulate',
-      // the proper extensions will be added
-      fileName: 'p5.modulate',
+      name: 'p5Modulate',
+      fileName: (format) => `p5.modulate.${format}.js`,
+      formats: ['es', 'umd'],
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
       external: ['p5'],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
         globals: {
           p5: 'p5',
         },
